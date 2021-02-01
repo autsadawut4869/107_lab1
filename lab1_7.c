586326df50889eac35ce1d35ef0d82dbe2a5f769
autsadawut4869 <78350629+autsadawut4869@users.noreply.github.com>
2021-02-01 20:00:35 +0700

    edit

---
+++ lab1_7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    float n;
+    scanf("%f",&n);
+    if(n>=0.00&&n<=11.59){
+        printf("%.2f a.m",n);
+    }
+    else
+    {
+        printf("%.2f p.m",n);
+    }
+
+    return 0;
+}
